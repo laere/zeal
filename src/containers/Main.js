875eacdef981e75239395f7f27ecd82fb940a92a
Zack Bostian <bostianz@gmail.com>
2016-04-04 13:24:58 -0400

    import scss into app

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-
 import ArtistContainer from '../Artist/ArtistContainer';
 import SearchbarContainer from '../Search/SearchbarContainer';
-
+import '../../scss/main.scss';
 
 export default class Main extends Component {
   render() {
